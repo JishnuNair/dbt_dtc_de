74a8952b2821fd35380435e10290d0cb8ad56b8b
Jishnu Nair <nair.jishnu2016@gmail.com>
2022-02-19 00:47:08 +0000

    removed de-duplication steps for stg_fhv_tripdata

--- models/staging/stg_fhv_tripdata.sql
+++ models/staging/stg_fhv_tripdata.sql
@@ -1,12 +1,5 @@
 {{ config(materialized='view') }}
 
-with tripdata as 
-(
-  select *,
-    row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
-  from {{ source('staging','fhv_tripdata') }}
-  where dispatching_base_num is not null 
-)
 select
 --identifiers
 {{ dbt_utils.surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
@@ -21,9 +14,7 @@ cast(dropoff_datetime as timestamp) as dropoff_datetime,
 SR_Flag as shared_ride_flag
 
 from
-tripdata
-where rn = 1
-and PULocationID != 0 and DOLocationID != 0
+{{ source('staging','fhv_tripdata') }}
 
 {% if var('is_test_run', default=false) %}
 
